3aa4351cd8797f311edb643b9a4ee67a51645c27
BallikuraRam <tr20@sharpgurus-usa.com>
2023-01-20 15:15:04 +0530

    keycloak changes

--- order-service/src/main/java/com/programmer/controller/OrderController.java
+++ order-service/src/main/java/com/programmer/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.programmer.controller;
 
 import com.programmer.dto.OrderRequest;
 import com.programmer.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +12,17 @@ public class OrderController {
 
     private final OrderService orderService ;
 
-    @PostMapping @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallBackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest)
     {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallBackMethod(OrderRequest orderRequest , RuntimeException runtimeException)
+    {
+        return "Oops ! somthing went wrong please try after some time!";
     }
 }
